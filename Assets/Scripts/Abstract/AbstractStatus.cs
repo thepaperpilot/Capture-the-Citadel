24f11b5295489bacb7f9a3c74e9c2ec752b93bac
Anthony Lawn <thepaperpilot@gmail.com>
2019-10-07 14:00:49 -0500

    Added statuses manager

---
+++ Assets/Scripts/Abstract/AbstractStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractStatus : MonoBehaviour
+{
+    public int priority;
+
+    public bool affectsStrength;
+    public bool affectsDexterity;
+
+    public int ModifyStrength(int currentStrength) {
+        return currentStrength;
+    }
+
+    public int ModifyDexterity(int currentDexterity) {
+        return currentDexterity;
+    }
+}
