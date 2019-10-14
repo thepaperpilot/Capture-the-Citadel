baf7d1ea19baf36224caab66c1773e4b77b71bd3
Anthony Lawn <thepaperpilot@gmail.com>
2019-10-14 14:09:11 -0500

    Made player and enemy spawns have icons

--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -57,38 +57,4 @@ public class CombatManager : MonoBehaviour
             ActionsManager.Instance.AddToBottom(new EndTurnAction());
         }
     }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmos() {
-        if (playerSpawnPoint != null)
-            drawString("P", playerSpawnPoint.position);
-        for (int i = 0; i < enemySpawnPoints.Length; i++) {
-            drawString(i.ToString(), enemySpawnPoints[i].position);
-        }
-    }
-
-    // Taken from https://gist.github.com/Arakade/9dd844c2f9c10e97e3d0
-    static public void drawString(string text, Vector3 worldPos, Color? colour = null) {
-        UnityEditor.Handles.BeginGUI();
-
-        var restoreColor = GUI.color;
-
-        if (colour.HasValue) GUI.color = colour.Value;
-        var view = UnityEditor.SceneView.currentDrawingSceneView;
-        Debug.Log(worldPos);
-        Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
-
-        if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
-        {
-            GUI.color = restoreColor;
-            UnityEditor.Handles.EndGUI();
-            return;
-        }
-
-        Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
-        GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height, size.x, size.y), text);
-        GUI.color = restoreColor;
-        UnityEditor.Handles.EndGUI();
-    }
-#endif
 }
