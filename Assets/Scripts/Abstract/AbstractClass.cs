0b80e15b95f1e58da8ac6df7cbb214a326e01c3c
Anthony Lawn <thepaperpilot@gmail.com>
2019-11-06 15:07:12 -0600

    Added Class (but no class seletion yet)

---
+++ Assets/Scripts/Abstract/AbstractClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Classes/Generic Class")]
+public class AbstractClass : ScriptableObject
+{
+    public int startingHealth;
+    [Space, InlineEditor(InlineEditorObjectFieldModes.Foldout)]
+    [AssetSelector(FlattenTreeView=true, DrawDropdownForListElements=false, IsUniqueList=false)]
+    public List<AbstractCard> startingDeck;
+    [Space, InlineEditor(InlineEditorObjectFieldModes.Foldout)]
+    [AssetSelector(FlattenTreeView=true, DrawDropdownForListElements=false)]
+    public List<AbstractRelic> startingRelics;
+}
